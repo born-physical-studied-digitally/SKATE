cd2c2fb3c882593d9cf12edce99bf15ebd018104
Brian Kim <160251900+briankim0629@users.noreply.github.com>
2025-04-18 00:32:01 -0500

    .

--- run_tests.py
+++ run_tests.py
@@ -13,7 +13,7 @@ for (i, filename) in enumerate(filenames):
   print("\nimage %s of %s" % (i, num_to_process))
   local_path = test_dir+filename
   # call(["aws s3 cp \"s3://WWSSN_Scans/"+filename+"\" ./"+local_path+" --region us-east-1 --profile seismo"], shell=True)
-
+#
   print("running test_meanlines_and_roi.sh")
   call(["bash test_meanlines_and_roi_s3.sh "+filename+" "+local_path], shell=True)
 
